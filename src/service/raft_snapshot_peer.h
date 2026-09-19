6d3c35c8af37790d02e39e77c9f22ca1be4bcbf0
Qi Gao <qi.gao2012@gmail.com>
2026-09-19 22:20:06 +0800

    feat(snapshot): bind peer to raft group

--- src/service/raft_snapshot_peer.h
+++ src/service/raft_snapshot_peer.h
@@ -14,6 +14,7 @@ typedef int (*tr_raft_snapshot_payload_enqueue_fn)(
 typedef struct tr_raft_snapshot_peer_config {
     tr_raft_node_id_t self_id;
     tr_raft_node_id_t peer_id;
+    tr_raft_group_id_t group_id;
     size_t max_snapshot_bytes;
     size_t chunk_size;
     size_t max_inflight_chunks;
