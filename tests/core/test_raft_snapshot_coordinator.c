5e353522c3867331ee44801e32e88a5a539eae4b
Qi Gao <qi.gao2012@gmail.com>
2026-09-21 12:50:28 +0800

    test: bound buffered snapshot receiver

--- tests/core/test_raft_snapshot_coordinator.c
+++ tests/core/test_raft_snapshot_coordinator.c
@@ -103,6 +103,7 @@ spec("raft snapshot coordinator")
         coordinator_config.emit_context = &emitted;
         receiver_config.self_id = 2U;
         receiver_config.max_snapshot_bytes = 1024U;
+        receiver_config.max_buffered_snapshot_bytes = 1024U;
         receiver_config.install = snapshot_capture_install;
         receiver_config.install_context = &installed;
 
